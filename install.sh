fc08d27e41d9d8a221c783d72954e87d16d27a4c
onodera_takumi <takumi.onodera@drecom.co.jp>
2016-07-23 19:51:19 +0900

    oh-my-zshを辞める。spacemacsを使い始める

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
+if [ "$(uname)" == 'Darwin' ]; then
+  ln -s ~/dotfiles/.vimrc ~/.vimrc
+  ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neobundle_install.sh && sh ./neobundle_install.sh
+  ln -s ~/dotfiles/.zshrc ~/.zshrc
+  ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+  ln -s ~/dotfiles/.gitignore ~/.gitignore
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap d12frosted/emacs-plus
+  brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
+  brew linkapps
+  mv ~/.emacs.d ~/.emacs.bak 
+  git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
